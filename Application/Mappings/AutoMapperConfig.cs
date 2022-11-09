f03082836b582b97695d16ededdb9fccaf38f008
LAPTOP-B38HVQTF\Krzysztof <KrzysztofFrankow@outlook.com>
2022-11-09 19:50:41 +0100

    Created initial solution

---
+++ Application/Mappings/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using Application.Dto;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappings
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg =>
+            {
+                #region Notes
+
+                cfg.CreateMap<Note, NoteDto>();
+                cfg.CreateMap<CreateNoteDto, Note>();
+                cfg.CreateMap<UpdateNoteDto, Note>();
+
+                #endregion
+            }).CreateMapper();
+    }
+}
---
+++ Infrastructure/Data/MyNotesContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Data
+{
+    public class MyNotesContext : DbContext
+    {
+        public MyNotesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>().ToTable("Notes");
+            modelBuilder.Entity<Note>().HasKey(x => x.Id);
+            modelBuilder.Entity<Note>()
+                .Property(x => x.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+            modelBuilder.Entity<Note>()
+                .Property(x => x.Content)
+                .HasMaxLength(2000);
+        }
+    }
+}

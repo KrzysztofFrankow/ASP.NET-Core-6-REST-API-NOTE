d159c79a3e570ba73dab9f38cc4369bc2bfe6b85
LAPTOP-B38HVQTF\Krzysztof <KrzysztofFrankow@outlook.com>
2022-11-09 20:02:53 +0100

    Added dummy testing endpoint

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+app.Map("/hello", () => "hello");
+
 // Configure the HTTP request pipeline.
 
 app.UseDeveloperExceptionPage();
